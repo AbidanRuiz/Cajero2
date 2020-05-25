53ac45441db46ad330ac8cba8c98dae342e6bcb3
AbidanRuiz <ruizhuacca@upeu.edu.pe>
2020-05-25 13:10:16 -0500

    Cajero2

---
+++ JS/Javascript.js
@@ -0,0 +1,153 @@
+
+document.getElementById("n1").addEventListener("click", n1);
+document.getElementById("n2").addEventListener("click", n2);
+document.getElementById("n3").addEventListener("click", n3);
+document.getElementById("n4").addEventListener("click", n4);
+document.getElementById("n5").addEventListener("click", n5);
+document.getElementById("n6").addEventListener("click", n6);
+document.getElementById("n7").addEventListener("click", n7);
+document.getElementById("n8").addEventListener("click", n8);
+document.getElementById("n9").addEventListener("click", n9);
+document.getElementById("n0").addEventListener("click", n0);
+
+
+function n1() {
+
+    let actual = document.getElementById('resultado').innerHTML;
+    let sumado = document.getElementById("n1").innerHTML;
+    document.getElementById('resultado').innerHTML = actual + sumado
+
+}
+function n2() {
+
+    let actual = document.getElementById('resultado').innerHTML;
+    let sumado = document.getElementById("n2").innerHTML;
+    document.getElementById('resultado').innerHTML = actual + sumado
+
+}
+function n3() {
+
+    let actual = document.getElementById('resultado').innerHTML;
+    let sumado = document.getElementById("n3").innerHTML;
+    document.getElementById('resultado').innerHTML = actual + sumado
+
+}
+
+function n4() {
+
+    let actual = document.getElementById('resultado').innerHTML;
+    let sumado = document.getElementById("n4").innerHTML;
+    document.getElementById('resultado').innerHTML = actual + sumado
+
+}
+
+function n5() {
+
+    let actual = document.getElementById('resultado').innerHTML;
+    let sumado = document.getElementById("n5").innerHTML;
+    document.getElementById('resultado').innerHTML = actual + sumado
+
+}
+
+function n6() {
+
+    let actual = document.getElementById('resultado').innerHTML;
+    let sumado = document.getElementById("n6").innerHTML;
+    document.getElementById('resultado').innerHTML = actual + sumado
+
+}
+
+function n7() {
+
+    let actual = document.getElementById('resultado').innerHTML;
+    let sumado = document.getElementById("n7").innerHTML;
+    document.getElementById('resultado').innerHTML = actual + sumado
+
+}
+function n8() {
+
+    let actual = document.getElementById('resultado').innerHTML;
+    let sumado = document.getElementById("n8").innerHTML;
+    document.getElementById('resultado').innerHTML = actual + sumado
+
+}
+
+function n9() {
+
+    let actual = document.getElementById('resultado').innerHTML;
+    let sumado = document.getElementById("n9").innerHTML;
+    document.getElementById('resultado').innerHTML = actual + sumado
+
+}
+
+function n0() {
+
+    let actual = document.getElementById('resultado').innerHTML;
+    let sumado = document.getElementById("n0").innerHTML;
+    document.getElementById('resultado').innerHTML = actual + sumado
+
+}
+
+
+
+function login() {
+
+    var pass = document.getElementById('variables').value;
+  
+
+    if (pass = "12345678") {
+
+        alert("Contraseña correcta...");
+
+        document.getElementById("m1").style.display = "none";
+        document.getElementById("m2").style.display = "block";
+
+    } else{
+
+        alert("Contraseña incorrecta...");
+        document.getElementById("Mensaje").innerHTML = "Contraseña incorrecta ..."
+
+
+    }
+}
+
+function add() {
+
+    var num = document.getElementById('resultado');
+    alert("Hola");
+    function add(n1) {
+
+        var x = num.value;
+        num.value = x + n1;
+
+    }
+
+    function cancelar() {
+
+
+        oper.value = "";
+        document.getElementById("Mensaje").innerHTML = "";
+
+
+    }
+    
+
+    function limpiar() {
+
+
+        document.getElementById('n1').value ="";
+        document.getElementById('n1').focus();
+
+    }
+    function deshabilitar() {
+
+
+        document.getElementById('n2').disabled = true;
+        document.getElementById('Quitar').disabled = true;
+
+    }
+
+
+
+
+}
